b24a7656ea4e998179cd977fc81955084d6bfd9e
Keith Carangelo <mail@kcaran.com>
2023-01-18 15:22:17 -0500

    Finished Day 24!

--- day24.pl
+++ day24.pl
@@ -17,6 +17,61 @@ use Path::Tiny;
 	'sw' => [ -1, -1 ],
 	);
 
+  sub black {
+    my ($self, $row, $col) = @_;
+
+    return $self->{ map }{ "$row,$col" } ? 1 : 0;
+   }
+
+  sub neighbors {
+    my ($self, $tile) = @_;
+
+    my @pos = split( ',', $tile );
+    my $blacks = 0;
+
+    for my $off (values %moves) {
+      $blacks += 1 if ($self->black( $pos[0] + $off->[0], $pos[1] + $off->[1] ));
+     }
+
+    return $blacks;
+   }
+
+  sub day {
+    my ($self) = @_;
+
+    my %flips;
+    for my $tile (keys %{ $self->{ map } }) {
+      my $blacks = $self->neighbors( $tile );
+      $flips{ $tile } = 1 if ($blacks == 0 || $blacks > 2);
+
+      my @pos = split( ',', $tile );
+      for my $off (values %moves) {
+        my $n = ($pos[0] + $off->[0]) . "," . ($pos[1] + $off->[1]);
+        next if $self->{ map }{ $n };
+        $flips{ $n } = 1 if ($self->neighbors( $n ) == 2);
+       }
+     }
+
+    for my $f (keys %flips) {
+      $self->flip( $f );
+     }
+
+    return;
+   }
+
+  sub flip {
+    my ($self, $tile) = @_;
+
+    if ($self->{ map }{ $tile }) {
+      delete $self->{ map }{ $tile };
+     }
+    else {
+      $self->{ map }{ $tile } = 1;
+     }
+
+    return;
+   }
+
   sub move {
     my ($self, $inst) = @_;
 
@@ -27,13 +82,7 @@ use Path::Tiny;
       $pos->[1] += $off->[1];
      }
 
-    my $tile = join( ',', @{ $pos } );
-    if ($self->{ map }{ $tile }) {
-      delete $self->{ map }{ $tile };
-     }
-    else {
-      $self->{ map }{ $tile } = 1;
-     }
+    $self->flip( "$pos->[0],$pos->[1]" );
 
     return;
    }
@@ -75,5 +124,12 @@ for my $inst (@{ $floor->{ inst } }) {
 
 my $black = %{ $floor->{ map } };
 print "There are $black black tiles\n";
+
+for my $i (1 .. 100) {
+  $floor->day();
+ }
+
+$black = %{ $floor->{ map } };
+print "After 100 days, there are $black black tiles\n";
 
 exit;
